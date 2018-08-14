919c6c743942e2b62fe0837282e4d462a79e1974
Henrique Sant'Anna <hsantanna@gmail.com>
2018-08-14 16:38:45 -0300

    função para gerar códigos aleatoriamente

--- encurtador/models.py
+++ encurtador/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+import random
 
+def gerador_codigo(tamanho=6, caracteres="abcdefghijklmnopqrstuvwxyz"):
+    return "".join(random.choice(caracteres) for _ in range(tamanho) )
+    # novo_código = ""
+    # for _ in range(tamanho):
+    #     novo_código += random.choice(caracteres)
+    # return novo_código
 
 # Create your models here.
 class URL(models.Model):
@@ -10,6 +17,10 @@ class URL(models.Model):
                                  default="codigopadrao",)
     atualizado = models.DateTimeField(auto_now=True)
     criado = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        self.shortcode = gerador_codigo()
+        super(URL,self).save(*args, **kwargs)
 
     # método que retorna string com identificação do objeto
     def __str__(self):
---
+++ encurtador/utils.py
@@ -0,0 +1,18 @@
+import random
+import string
+
+def criar_shortcode(instance, tamanho=6):
+    novo_código = gerador_codigo(tamanho)
+    Classe = instance.__class__
+    consulta_existe = Classe.objects().filter(shortcode=novo_código)
+    if consulta_existe:
+        return criar_shortcode(tamanho=tamanho)
+    return novo_código
+
+def gerador_codigo(tamanho=6, caracteres=string.ascii_letters
++string.digits):
+    return "".join(random.choice(caracteres) for _ in range(tamanho) )
+    # novo_código = ""
+    # for _ in range(tamanho):
+    #     novo_código += random.choice(caracteres)
+    # return novo_código
